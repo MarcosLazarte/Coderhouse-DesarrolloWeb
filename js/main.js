8cdc2cfc5750e61471f88c94efd3fb0ef95c0a0d
Marcos <marcos.e.lazarte@gmail.com>
2021-05-08 21:40:10 -0300

    agregado js sabado3

--- js/main.js
+++ js/main.js
@@ -1,21 +1,37 @@
 const btnSwitch = document.querySelector("#switch");
-const btnTitulo = document.querySelector("#titulo");
-const btnParrafo = document.querySelector("#parrafo");
-const btnStar = document.querySelector("#star");
+const mTitulo = document.querySelector("#titulo");
+const mParrafo = document.querySelector("#parrafo");
+const mParrafo2 = document.querySelector("#parrafo2");
+const mParrafo3 = document.querySelector("#parrafo3");
+const hNavNombre = document.querySelector("#nav__nombre");
+const hNavEstado = document.querySelector("#nav__estado");
+const hNavAnio = document.querySelector("#navAbajo__time");
+
+const hStar = document.querySelector("#star");
+const mAky = document.querySelector("#imagenAky"); //revisar
 let animado = document.querySelectorAll(".animado");
 var alto = (window.innerHeight);
 
 btnSwitch.addEventListener('click', () =>{
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
-    btnTitulo.classList.toggle('dark');
-    btnParrafo.classList.toggle('dark');
+    mTitulo.classList.toggle('dark');
+    mParrafo.classList.toggle('dark');
+    mParrafo2.classList.toggle('dark');
+    mParrafo3.classList.toggle('dark');
+
+    mAky.classList.toggle('dark'); //revisar
+    hNavNombre.classList.toggle('dark');
+    hNavEstado.classList.toggle('dark');
+    hNavAnio.classList.toggle('dark');
+    hStar.classList.toogle('dark');
+
 });
-btnStar.addEventListener('click', () =>{
+hStar.addEventListener('click', () =>{
     document.body.classList.toggle('color1');
-    btnTitulo.classList.toggle('color1');
-    btnParrafo.classList.toggle('color1');
-    btnStar.classList.toogle('color1')
+    mTitulo.classList.toggle('color1');
+    mParrafo.classList.toggle('color1');
+    hStar.classList.toogle('color1');
 });
 function mostrarScroll() {
     let scrollTop = document.documentElement.scrollTop;
